c50ec0eb68857bfb0d1503686e16d1710e656aad
Dallas Caley <dallascaley@gmail.com>
2024-11-18 18:50:08 -0800

    Initial code, page 7 of instructions

---
+++ index.js
@@ -0,0 +1,42 @@
+// Add required packages
+const express = require("express");
+const app = express();
+require('dotenv').config();
+
+// Set up EJS
+app.set("view engine", "ejs");
+
+// Start listener
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server started (http://localhost:3000/) !");
+});
+
+// Add database package and connection string (can remove ssl)
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  },
+  max: 2
+});
+
+app.get("/", (req, res) => {
+  const sql = "SELECT * FROM PRODUCT ORDER BY PROD_ID";
+  pool.query(sql, [], (err, result) => {
+      let message = "";
+      let model = {};
+      if(err) {
+          message = `Error - ${err.message}`;
+      } else {
+          message = "success";
+          model = result.rows;
+      };
+      res.render("index", {
+          message: message,
+          model : model
+      });
+  });
+});
+
+
